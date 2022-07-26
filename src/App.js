2b15bf735bfdb103c95ed0dd2f24e0dad0d1b897
Nora <nora@O3s-MBP-8.fios-router.home>
2022-07-26 07:43:13 -0400

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import PostFeed from './views/PostFeed';
+import Details from './views/Details';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <header>
-        <nav>Universe React</nav>
+        <nav><Link to="/">Universe React</Link></nav>
       </header>
       <main>
-        <PostFeed />
+        <Routes>
+          <Route path="/" element={<PostFeed />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
       </main>
-    </div>
+    </BrowserRouter>
   );
 }
 
